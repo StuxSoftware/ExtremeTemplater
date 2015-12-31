417abbe693c271886c00d4c5ccb36bff919da68f
StuxSoftware <netzmit@gmail.com>
2015-12-31 04:41:45 +0100

    Lol. gfind was renamed

--- Utils.lua
+++ Utils.lua
@@ -26,7 +26,7 @@
     local pat = "(.-)" .. delim .. "()"
     local nb = 0
     local lastPos
-    for part, pos in string.gfind(str, pat) do
+    for part, pos in string.gmatch(str, pat) do
         nb = nb + 1
         result[nb] = part
         lastPos = pos
